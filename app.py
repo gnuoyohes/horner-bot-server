bb2a8e0c1282a2f637a18816a89733ae43cd07c2
Seho Young <gnuoyohes@gmail.com>
2025-03-31 15:36:46 -0400

    add several UI elements

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import base64
 import psutil
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socket = SocketIO(app)
 
 thread = None
 thread_lock = Lock()
@@ -15,9 +15,12 @@ thread_event = Event()
 
 client_count = 0
 
-running = False
-
-use_yolo = False
+state = {
+    'laser_running': False,
+    'use_yolo': False,
+    'minimum_contour_area': 1500,
+    'laser_coords': [0, 0] # [0, 0] is top left, [1, 1] is bottom right of #laser-container
+}
 
 camera = cv2.VideoCapture(0)
 backsub = cv2.createBackgroundSubtractorMOG2(history=200, varThreshold=16, detectShadows=False)
@@ -25,35 +28,33 @@ backsub = cv2.createBackgroundSubtractorMOG2(history=200, varThreshold=16, detec
 model = YOLO("yolov8n.pt")
 
 def detect_objects_yolo(frame):
-    results = model.track(frame, persist=True)
+    results = model.track(frame, persist=False)
     return results[0].plot()
 
 def detect_objects_backsub(frame):
     fg_mask = backsub.apply(frame)
-    # Find contours
-    contours, hierarchy = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # print(contours)
-    # frame_ct = cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
     retval, mask_thresh = cv2.threshold( fg_mask, 180, 255, cv2.THRESH_BINARY)
     # set the kernal
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     # Apply erosion
     mask_eroded = cv2.morphologyEx(mask_thresh, cv2.MORPH_OPEN, kernel)
-    # min_contour_area = 1000  # Define your minimum area threshold
-    # large_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_contour_area]
-    biggest_contour = None
-    for cnt in contours:
-        if biggest_contour is None or cv2.contourArea(cnt) > cv2.contourArea(biggest_contour):
-            biggest_contour = cnt
-    
+    # Find contours
+    contours, hierarchy = cv2.findContours(mask_eroded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     frame_out = frame.copy()
-    if biggest_contour is not None:
-        x, y, w, h = cv2.boundingRect(biggest_contour)
-        frame_out = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 200), 3)
 
-    # for cnt in large_contours:
-    #     x, y, w, h = cv2.boundingRect(cnt)
+    # biggest_contour = None
+    # for cnt in contours:
+    #     if biggest_contour is None or cv2.contourArea(cnt) > cv2.contourArea(biggest_contour):
+    #         biggest_contour = cnt
+    
+    # if biggest_contour is not None:
+    #     x, y, w, h = cv2.boundingRect(biggest_contour)
     #     frame_out = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 200), 3)
+
+    large_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > state['minimum_contour_area']]
+    for cnt in large_contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        frame_out = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 200), 3)
 
     return frame_out
 
@@ -63,13 +64,13 @@ def generate_frames(event):
         while event.is_set():
             success, frame = camera.read()
             if success:
-                if use_yolo:
+                if state['use_yolo']:
                     annotated_frame = detect_objects_yolo(frame)
                 else:
                     annotated_frame = detect_objects_backsub(frame)
                 _, buffer = cv2.imencode('.jpg', annotated_frame)
                 frame_b64 = base64.b64encode(buffer).decode('utf-8')
-                socketio.emit('video_frame', {'image': frame_b64})
+                socket.emit('video_frame', {'image': frame_b64})
             else:
                 break
     finally:
@@ -79,54 +80,33 @@ def generate_frames(event):
 @app.route("/")
 def index():
     return render_template('index.html')
-
-@app.route("/get_status", methods=['GET'])
-def get_status():
-    global running
-    return "running" if running else "stopped"
-
-@app.route("/start", methods=['POST'])
-def start():
-    global running
-    if not running:
-        running = True
-        return "success"
-    else:
-         return "error"
     
-@app.route("/stop", methods=['POST'])
-def stop():
-    global running
-    if running:
-        running = False
-        return "success"
-    else:
-         return "error"
-    
-@app.route('/system_info', methods=['GET'])
-def system_info():
-    stats = {
-        'cpu_percent': psutil.cpu_percent(),
-        'memory_percent': psutil.virtual_memory().percent,
-        'disk_percent': psutil.disk_usage('/').percent
-    }
-    return stats
+# @app.route('/system_info', methods=['GET'])
+# def system_info():
+#     stats = {
+#         'cpu_percent': psutil.cpu_percent(),
+#         'memory_percent': psutil.virtual_memory().percent,
+#         'disk_percent': psutil.disk_usage('/').percent
+#     }
+#     return stats
 
-@socketio.on('connect')
+@socket.on('connect')
 def handle_connect():
     client_id = request.sid
     print(f'Client connected with id: {client_id}')
     global thread, client_count
     client_count += 1
 
+    socket.emit('state', state)
+
     if client_count == 1:
         with thread_lock:
             if thread is None:
                 thread_event.set()
-                thread = socketio.start_background_task(generate_frames, thread_event)
+                thread = socket.start_background_task(generate_frames, thread_event)
                 print('Video stream thread started')
 
-@socketio.on('disconnect')
+@socket.on('disconnect')
 def handle_disconnect():
     client_id = request.sid
     print(f'Client disconnected with id: {client_id}')
@@ -141,13 +121,19 @@ def handle_disconnect():
                 thread = None
                 print('Video stream thread stopped')
 
-@socketio.on('switch_object_detection')
-def switch_object_detection():
-    global use_yolo
-    use_yolo = not use_yolo
-    alg = 'yolo' if use_yolo else 'backsub'
-    print(f"Object detection algorithm switched to {alg}")
+@socket.on('update_state')
+def update_state(params):
+    global state
+    for p in params:
+        if p == 'minimum_contour_area':
+            new_val = float(params[p])
+        else:
+            new_val = params[p]
+        state[p] = new_val
+        print(f"State property \"{p}\" updated to {new_val}")
+    socket.emit('state', params)
+    
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host="0.0.0.0")
+    socket.run(app, debug=True, host="0.0.0.0")
--- static/script.js
+++ static/script.js
@@ -1,11 +1,114 @@
 // var socket = io.connect('10.0.0.39:5000');
-var socket = io();
+var socket = io()
 
 socket.on('video_frame', function(data) {
-    var videoElement = document.getElementById('video');
-    videoElement.src = 'data:image/jpeg;base64,' + data.image;
+    $('#videoStream').attr('src', 'data:image/jpeg;base64,' + data.image);
 });
 
-function switchObjectDetection() {
-    socket.emit('switch_object_detection')
+socket.on('state', function(data) {
+    for (const key in data) {
+        switch (key) {
+            case 'use_yolo':
+                $('#yoloSwitch').prop('checked', data[key])
+                break
+            case 'laser_running':
+                var laserBtn = $('#btn_start_laser')
+                var laserPoint = $('#laser-point')
+                if (data[key]) {
+                    laserBtn.addClass('btn-danger')
+                    laserBtn.removeClass('btn-primary')
+                    laserBtn.text('Stop Laser')
+                    laserPoint.css("background-color", "#dc3545");
+                }
+                else {
+                    laserBtn.addClass('btn-primary')
+                    laserBtn.removeClass('btn-danger')
+                    laserBtn.text('Start Laser')
+                    laserPoint.css("background-color", "#0d6efd");
+                }
+                break
+            case 'minimum_contour_area':
+                var slider = $('#minContourAreaSlider')
+                var sliderValLabel = $('#minContourAreaVal')
+                slider.val(data[key])
+                sliderValLabel.text(data[key])
+                break
+            case 'laser_coords':
+                if (!dragging) {
+                    var coordsX = data[key][0]
+                    var coordsY = data[key][1]
+                    laserPositionScaled = [coordsX, coordsY]
+                    updateLaserPosition(coordsX, coordsY)
+                }
+        }
+    }
+});
+
+function updateState(params) {
+    socket.emit('update_state', params)
 }
+
+function handleYoloCheckbox(cb) {
+    updateState({'use_yolo': cb.checked})
+}
+
+function handleLaser(cb) {
+    const isRunning = cb.innerHTML == 'Start Laser';
+    updateState({'laser_running': isRunning})
+}
+
+function handleMinContourAreaSlider(cb) {
+    const sliderVal = cb.value
+    console.log(sliderVal)
+    updateState({'minimum_contour_area': sliderVal})
+}
+
+var laserPosition = { x: 0, y: 0 }
+var laserPositionScaled = [0, 0]
+const laserWidth = $('#laser-point').outerWidth()
+const laserHeight = $('#laser-point').outerHeight()
+var containerWidth = $('#laser-container').outerWidth()
+var containerHeight = $('#laser-container').outerHeight()
+var dragging = false
+
+interact('#laser-point').draggable({
+    listeners: {
+        start (event) {
+            console.log(event.type, event.target)
+            dragging = true
+        },
+        move (event) {
+            laserPosition.x += event.dx
+            laserPosition.y += event.dy
+            
+            let posScaled = [
+                (laserPosition.x + 1) / (containerWidth - laserWidth),
+                (laserPosition.y + 1) / (containerHeight - laserHeight)
+            ]
+            // console.log(posScaled)
+            laserPositionScaled = posScaled
+            updateState({'laser_coords': posScaled})
+            $('#laser-point').css('transform', `translate(${laserPosition.x}px, ${laserPosition.y}px)`)
+        },
+        end (event) {
+            dragging = false
+        }
+    },
+    modifiers: [
+        interact.modifiers.restrictRect({
+            restriction: 'parent',
+        })
+    ]
+})
+
+function updateLaserPosition(newXScaled, newYScaled) {
+    laserPosition.x = newXScaled * (containerWidth - laserWidth) - 1
+    laserPosition.y = newYScaled * (containerHeight - laserHeight) - 1
+    $('#laser-point').css('transform', `translate(${laserPosition.x}px, ${laserPosition.y}px)`)
+}
+
+window.addEventListener('resize', function() {
+    containerWidth = $('#laser-container').outerWidth()
+    containerHeight = $('#laser-container').outerHeight()
+    updateLaserPosition(laserPositionScaled[0], laserPositionScaled[1])
+});
